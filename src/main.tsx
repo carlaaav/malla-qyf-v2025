e8bcb08a871a5a18b4d674a1e26b12d40be9accd
carlaaav <carlantil.lobos@gmail.com>
2025-07-25 01:42:32 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// Configuración responsive y touch
+import { setViewportHeight } from './utils/viewportUtils'
+
+// Inicializar ajustes de viewport
+setViewportHeight()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
